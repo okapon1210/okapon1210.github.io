04e9d414e4dbcb5a0782323dd335ba93be26d62c
okapon1210 <okapon1210@gmail.com>
2021-09-01 23:58:29 +0900

    value取得するの忘れてた

--- js/index.js
+++ js/index.js
@@ -1,22 +1,22 @@
 // ページの初期化
-(function init(){
+(function init() {
     let createButton = document.getElementById("createButton")
-    createButton.addEventListener("click", {eventType: "create", handleEvent: connect})
+    createButton.addEventListener("click", { eventType: "create", handleEvent: connect })
     dispRoom()
     document.getElementById("refresh").onclick = dispRoom
 }())
 
 // ビデオ通話接続処理
-async function connect(e){
+async function connect(e) {
     let roomId
-    if (e.currentTarget){
+    if (e.currentTarget) {
         roomId = e.currentTarget.value
-    }else{
+    } else {
         roomId = null
     }
 
     // カメラとマイクを取得
-    let stream = await navigator.mediaDevices.getUserMedia({video: true, audio: true}).catch(window.alert)
+    let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true }).catch(window.alert)
     document.getElementById('localVideo').srcObject = stream
 
     // PeerConnectionを作成
@@ -30,49 +30,49 @@ async function connect(e){
 
     // Trackが追加された時のハンドラ
     // VideoTrackが追加された時にVideoエレメントを生成する
-    pc.ontrack = function(event){
+    pc.ontrack = function (event) {
         console.log("ontrack")
-        if(event.track.kind === 'audio'){
+        if (event.track.kind === 'audio') {
             return
         }
-    
+
         let el = document.createElement(event.track.kind)
         el.srcObject = event.streams[0]
         el.autoplay = true
         el.controls = true
         el.classList.add("remoteVideo")
         document.getElementById('remoteVideos').appendChild(el)
-    
+
         event.track.onmute = () => {
             el.play()
         }
-    
+
         event.streams[0].onremovetrack = () => {
-            if(el.parentNode){
+            if (el.parentNode) {
                 el.parentNode.removeChild(el)
             }
         }
     }
 
     // ICECandidateを送信
-    pc.onicecandidate = function(event){
-        ws.send(JSON.stringify({event: "candidate", data: JSON.stringify(event.candidate.toJSON())}))
+    pc.onicecandidate = function (event) {
+        ws.send(JSON.stringify({ event: "candidate", data: JSON.stringify(event.candidate.toJSON()) }))
     }
 
     // WebSocketの作成
     let addr = document.getElementById("address")
-    let ws = new WebSocket("ws://" + addr +"/ws")
+    let ws = new WebSocket("ws://" + addr.value + "/ws")
 
     // WebSocketが接続できた時のハンドラ
     // WebSocketの接続を確認してからメッセージを送信する
     ws.onopen = () => {
         console.log(this.eventType + ": " + roomId)
-        ws.send(JSON.stringify({event: this.eventType, data: roomId}))
+        ws.send(JSON.stringify({ event: this.eventType, data: roomId }))
     }
 
     // WebSocketが切断されたときのハンドラ
     // PeerConnectionを閉じてVideoエレメントを削除する
-    ws.onclose = function(event){
+    ws.onclose = function (event) {
         window.alert("connection closed")
         pc.close()
         stream.getTracks().forEach(t => t.stop())
@@ -87,31 +87,31 @@ async function connect(e){
 
     // WebSocketがメッセージを受け取った時のハンドラ
     // SDPOfferを受け取ったらSDPAnswerを返してICECandidateの交換をする
-    ws.onmessage = function(event){
+    ws.onmessage = function (event) {
         console.log(JSON.parse(event.data))
         let msg = JSON.parse(event.data)
-        if(!msg){
+        if (!msg) {
             return console.log('faild to parse answer')
         }
 
-        switch(msg.event){
+        switch (msg.event) {
             case 'offer':
                 console.log("offer arrived")
                 let offer = JSON.parse(msg.data)
-                if (!offer){
+                if (!offer) {
                     return console.log('failed to parse answer')
                 }
                 pc.setRemoteDescription(offer)
                 pc.createAnswer().then(answer => {
                     console.log("create Answer")
                     pc.setLocalDescription(answer)
-                    ws.send(JSON.stringify({event: 'answer', data: JSON.stringify(answer)}))
+                    ws.send(JSON.stringify({ event: 'answer', data: JSON.stringify(answer) }))
                 })
                 return
-                
+
             case 'candidate':
                 let candidate = JSON.parse(msg.data)
-                if(!candidate){
+                if (!candidate) {
                     return console.log('failed to parse candidate')
                 }
 
@@ -120,23 +120,23 @@ async function connect(e){
     }
 
     // WebSocketがエラーを起こしたときのハンドラ
-    ws.onerror = function(event){
-        console.log("ERROR: "+event.data)
+    ws.onerror = function (event) {
+        console.log("ERROR: " + event.data)
     }
 
     // 切断ボタンにイベントを登録する
     document.getElementById("closeButton").onclick = () => {
-        ws.send(JSON.stringify({event: "close", data: ""}))
+        ws.send(JSON.stringify({ event: "close", data: "" }))
     }
 }
 
 // 部屋一覧の更新
-async function dispRoom(){
+async function dispRoom() {
     let roomlist = document.getElementById("roomlist")
     let elm = roomlist.cloneNode(false)
 
     let res = await getRoomList()
-    
+
     Object.keys(res).forEach((key) => {
         let roomRow = document.createElement("div")
         roomRow.classList.add("roomRow")
@@ -150,7 +150,7 @@ async function dispRoom(){
         connectionButton.classList.add("connectionButton")
         connectionButton.value = res[key]
         connectionButton.innerText = '接続'
-        connectionButton.addEventListener("click", {eventType: "join", handleEvent: connect})
+        connectionButton.addEventListener("click", { eventType: "join", handleEvent: connect })
         roomRow.appendChild(connectionButton)
 
         elm.appendChild(roomRow)
@@ -160,8 +160,8 @@ async function dispRoom(){
 }
 
 // サーバに部屋一覧を問い合わせて結果をJsonで返す
-async function getRoomList(){
+async function getRoomList() {
     let addr = document.getElementById("address")
-    let response = await fetch("https://"+addr+'/list')
+    let response = await fetch("https://" + addr.value + '/list')
     return response.json()
 }
